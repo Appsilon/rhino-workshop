e3fddbb8826c5978611c7b923296993cbac66fcb
Kamil Zyla <kamil@appsilon.com>
2022-10-10 13:52:49 +0200

    [2] Add dependencies

--- dependencies.R
+++ dependencies.R
@@ -1,2 +1,7 @@
 # This file allows packrat (used by rsconnect during deployment) to pick up dependencies.
+library(dplyr)
+library(echarts4r)
+library(htmlwidgets)
+library(reactable)
 library(rhino)
+library(tidyr)
